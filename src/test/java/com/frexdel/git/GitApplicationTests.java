070650019f0016972eba3d1f0176332f3f074dca
frexdel <frexdel@gmail.com>
2019-11-23 21:16:30 +0100

    Initial commit new Project

---
+++ src/test/java/com/frexdel/git/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frexdel.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
