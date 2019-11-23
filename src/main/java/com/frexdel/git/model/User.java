4eb5af2d6df96f66b2f4975add9ad9cdfabd9a1f
frexdel <frexdel@gmail.com>
2019-11-23 21:53:39 +0100

    Create quotes in user Class

--- src/main/java/com/frexdel/git/model/User.java
+++ src/main/java/com/frexdel/git/model/User.java
@@ -1,4 +1,28 @@
 package com.frexdel.git.model;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Observable;
+
 public class User {
+    private static String[] quotes = {"AAPL", "GOOG", "INTC", "BABA", "TSLA", "AIR.PA"};
+
+    public Observable<St
+    ock> getStock() {
+
+        return Observable.create(subscriber -> {
+            if (!subscriber.isUnsubscribed()) {
+                Arrays.stream(quotes)
+                        .map(quote -> getStock(quote, subscriber))
+                        .filter(Objects::nonNull)
+                        .forEach(stock -> {
+                            subscriber.onNext(stock);
+                            sleep(3000);
+                            subscriber.onError(new RuntimeException("exception"));
+                        });
+
+            }
+            subscriber.onCompleted();
+        });
+    }
 }
